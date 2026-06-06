a583daa8edca7fc3028b5c12d169bd5a4edbe88e
Reynald <reynaldochavarrias66@gmail.com>
2026-06-05 22:00:35 -0600

    roles y permiso

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -13,16 +13,14 @@ class Permission extends SpatiePermission
         'category_id'
     ];
 
-    protected $attributes = [
-        'guard_name' => 'sanctum'
-    ];
+   
 
     /**
      * Get the category that owns the permission.
      */
     public function category()
     {
-        return $this->belongsTo(\App\Models\PermissionCategory::class, 'category_id');
+        return $this->belongsTo(PermissionCategory::class, 'category_id');
     }
 
     /**
